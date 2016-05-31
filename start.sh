ec4a55645b307e92b8fe4b5ebd63b936f452b976
Evan Lezar <evan.lezar@zalando.de>
2016-05-31 21:45:59 +0200

    Also add username to ./home folder.

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 DOCKER_IMAGE=tf_devel
+USERNAME=$(whoami)
 
 # First ensure that the docker image is built.
 docker build --build-arg user_id=$(id -u) \
             --build-arg group_id=$(id -g) \
-            --build-arg user_name=$(whoami) \
+            --build-arg user_name=$USERNAME \
             -t $DOCKER_IMAGE .
 
 if [ "$?" -eq 0 ]; then
@@ -14,7 +15,7 @@ docker run --rm -ti \
 			-u $(id -u):$(id -g) \
 			-w /workspace \
 			-v $(pwd):/workspace \
-			-v $(pwd)/.home:/home/$(whoami) \
+			-v $(pwd)/.home/$USERNAME:/home/$USERNAME \
 			-p 8888:8888 -p 6006:6006 \
 			$DOCKER_IMAGE
 fi
