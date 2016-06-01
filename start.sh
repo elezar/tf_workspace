36ebb70a13a2994f4fc6509bacc7ab4b8ba03f9f
Evan Lezar <evan.lezar@zalando.de>
2016-06-01 14:49:09 +0200

    Add CPU and GPU versions of the docker files.

--- start.sh
+++ start.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
-DOCKER_IMAGE=tf_devel
+# Determine the name of the current user.
 USERNAME=$(whoami)
 
+DOCKER_IMAGE=tf_devel
+DOCKER_FILE=Dockerfile
+
+GPU=${GPU:-0}
+
+if [ "$GPU" != "1" ]
+then
+TAG=cpu
+DOCKER_EXE=docker
+else
+TAG=gpu
+DOCKER_EXE=nvidia-docker
+fi
+
+DOCKER_IMAGE=$DOCKER_IMAGE:$TAG
+DOCKER_FILE=$DOCKER_FILE.$TAG
+
+# Construct a hostname from the image name.
+DOCKER_HOSTNAME=${DOCKER_IMAGE//_/-}
+DOCKER_HOSTNAME=${DOCKER_HOSTNAME//:/-}
+
 # First ensure that the docker image is built.
-docker build --build-arg user_id=$(id -u) \
+$DOCKER_EXE build --build-arg user_id=$(id -u) \
             --build-arg group_id=$(id -g) \
             --build-arg user_name=$USERNAME \
-            -t $DOCKER_IMAGE .
+            -t $DOCKER_IMAGE -f $DOCKER_FILE .
 
+# If the build succeeded.
 if [ "$?" -eq 0 ]; then
-docker run --rm -ti \
-            -h "${DOCKER_IMAGE/_/-}" \
+# Create a temporary home folder. This ensures that the permissions are
+# correct.
+mkdir -p .home/$USERNAME
+
+$DOCKER_EXE run --rm -ti \
+            -h "$DOCKER_HOSTNAME" \
 			-u $(id -u):$(id -g) \
 			-w /workspace \
 			-v $(pwd):/workspace \
 			-v $(pwd)/.home/$USERNAME:/home/$USERNAME \
-			-p 8888:8888 -p 6006:6006 \
+			-p 8888:8888 \
+			-p 6006:6006 \
 			$DOCKER_IMAGE
 fi
